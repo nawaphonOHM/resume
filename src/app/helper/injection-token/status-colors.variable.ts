47feb484d9df4250f7d75c7f2a3d4e7537662a0a
Nawaphon Isarathanachaikul <nawaphon2539@gmail.com>
2026-09-02 23:12:42 +0700

    feat(core): add injection token for `STATUS_COLORS` with factory implementation

---
+++ src/app/helper/injection-token/status-colors.variable.ts
@@ -0,0 +1,18 @@
+import {InjectionToken} from '@angular/core';
+import type {STATUS_COLORS} from './status_colors.type.ts';
+
+
+/** Confirmed colors for available, limited, and unavailable UTC+7 periods. */
+export const statusColor = new InjectionToken<STATUS_COLORS>(
+  'StatusColor',
+  {
+    providedIn: 'root',
+    factory: () => {
+      return {
+        available: '#92C353',
+        limited: '#F7A600',
+        unavailable: '#D1D1D1',
+      }
+    }
+  }
+);
--- src/app/helper/injection-token/status_colors.type.ts
+++ src/app/helper/injection-token/status_colors.type.ts
@@ -1,12 +1,4 @@
 
-
-/** Confirmed colors for available, limited, and unavailable UTC+7 periods. */
-// export const STATUS_COLORS = {
-//   available: '#92C353',
-//   limited: '#F7A600',
-//   unavailable: '#D1D1D1',
-// } as const;
-
 /** Confirmed colors for available, limited, and unavailable UTC+7 periods. */
 export type STATUS_COLORS = {
   available: string;
